4d9c322bb7a2478793133586cf6e7b7661f4bab7
Igor Smelyanets <ismelyanets@yahoo.com>
2022-05-25 13:07:03 +0300

    resolve hw

---
+++ src/js/03-feedback.js
@@ -0,0 +1,42 @@
+
+import throttle from "lodash.throttle";
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    email: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+}
+
+
+refs.form.addEventListener("input", throttle(onFormInput, 500));
+refs.form.addEventListener("submit", onFormSubmit)
+
+const userData = {
+}
+
+onDataFromStorage();
+function onDataFromStorage() {
+    const formEl = JSON.parse(localStorage.getItem("feedback-form-state"));
+    
+    if (formEl.email) {
+        refs.email.value = formEl.email;
+    }
+    if (formEl.message) {
+        refs.textarea.value = formEl.message;
+    }
+}
+
+
+function onFormInput(e) {
+    userData[e.target.name] = e.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(userData));
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    e.target.reset();
+    localStorage.removeItem("feedback-form-state");
+    
+}
